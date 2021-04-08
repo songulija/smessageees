c26b52d1b0cd07bd046f76ae17e52e8563942a63
PC <lsongulija@gmail.com>
2021-04-08 18:58:34 +0200

    Sidebar component styled. Starting building sidebar chat component

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,18 +1,23 @@
-import { Avatar } from '@material-ui/core'
-import { Search } from '@material-ui/icons'
+import { Avatar, IconButton } from '@material-ui/core'
+import { RateReviewOutlined, Search } from '@material-ui/icons'
 import React from 'react'
 import './styles.css'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
+            {/* first we have sideba header in in sidebar input */}
             <div className='sidebar__header'>
-                <Avatar />
+                <Avatar className='sidebar__avatar' />
                 {/* Search box */}
                 <div className='sidebar__input'>
                     <Search />
                     <input type='text' placeholder='Search' />
                 </div>
+                {/* wrapping  RateReviewOutlined icon in IconButton so it will be clickable*/}
+                <IconButton variant='outlined' className='sidebar__inputButton'>
+                    <RateReviewOutlined />
+                </IconButton>
             </div>
 
             <div className='sidebar__chats'>
